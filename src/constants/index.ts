4e11a731d80178b067249430fa5008461760449e
David Arriaga <brayan.arriagaa@ug.edu.ec>
2021-09-21 08:22:30 -0500

    Changing the server url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ export enum ImgSize {
 	'TUMBNAIL' = 'w_50,h_50'
 }
 
-export const SERVER_URL = 'http://localhost:4500/api';
+export const SERVER_URL = 'https://spring-boot-david-1998.herokuapp.com/api';
 export const IMAGE_STORAGE_URL =
 	'https://res.cloudinary.com/dqvyxdjda/image/upload/w_860,h_860/v1631363076/';
 
